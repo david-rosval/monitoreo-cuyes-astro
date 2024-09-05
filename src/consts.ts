35069fe2493e73c7d101c777f174eefd14f27cd8
david-rosval <20b3010135@untels.edu.pe>
2024-09-05 16:59:09 -0500

    primera presentación

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Gestión de procesos TI';
+export const SITE_DESCRIPTION = 'Presentación de avance de proyecto de monitoreo de cuyes';
